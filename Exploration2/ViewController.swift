349d999eaf247dad987ce8ea5b60bb215135697e
Emilyyan <xw324@mail.missouri.edu>
2015-10-01 20:42:02 -0500

    Initial Commit

---
+++ Exploration2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exploration2
+//
+//  Created by Emily on 10/1/15.
+//  Copyright © 2015 Emily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
